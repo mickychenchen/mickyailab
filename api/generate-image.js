1e43a755066373948d7993d24cc29d388ece135f
mickychenchen <eastwinterga@hotmail.com>
2026-03-14 05:37:12 +0000

    fix: standardized api and include package-lock

--- api/generate-image.js
+++ api/generate-image.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 
 export default async function handler(req, res) {
     const { prompt, seed, width = 1024, height = 1024, model = 'flux' } = req.query;
